7cf7a705345d8b32906b13ad8610a2cf01220135
Weijian Zhang <weijian.z@hotmail.com>
2015-04-21 13:01:01 +0100

    fix depreciation warning

--- test/graphs.jl
+++ test/graphs.jl
@@ -2,11 +2,11 @@
 # test EvolvingGraph type
 a = [1,2,2,3,4,4,5];
 b = [2,3,3,4,6,2,1];
-times = [1.:7];
+times = [1.:7;];
 
 g = evolving_graph(a, b, times)
 @test num_nodes(g) == 6
-@test_approx_eq norm(nodes(g) - [1:6]) 0
+@test_approx_eq norm(nodes(g) - [1:6;]) 0
 @test num_edges(g) == 7
 edges(g)
 
@@ -20,6 +20,6 @@ for i = 1:3
     push!(As, rand(3,3))
 end
 
-times = [1:3]
+times = [1:3;]
 
 g3 = time_tensor(times, As)
