45827b14ee11f076b83a1a443f8c9bc62ead126f
Weijian Zhang <weijian.z@hotmail.com>
2015-04-27 09:44:48 +0100

    minor change of katz_centrality

--- src/algorithms/katz_centrality.jl
+++ src/algorithms/katz_centrality.jl
@@ -5,17 +5,19 @@
 # mode = :broadcast (broadcast centrality vector)
 #      = :receive (receive centrality vector)
 #      = :matrix (the communicability matrix)
-function katz_centrality(g::EvolvingGraph, α::Real = 0.3, β::Real = 0.2;
+function katz_centrality(g::EvolvingGraph, 
+                         α::Real = 0.3, 
+                         β::Real = 0.2;
                          mode::Symbol = :broadcast)
     n = num_nodes(g)
     ns = nodes(g)
     ts = timestamps(g) 
-    S = spzeros(Bool, n, n)
-    A = spzeros(Bool, n, n)
+    S = spzeros(Float64, n, n)
+    A = spzeros(Float64, n, n)
     I = speye(Float64, n)
     v = Array(Float64, n) 
-    Δt = 1
-    for t in ts 
+    Δt = 1.
+    for t in ts
         Δt += 0.01
         copy!(A, spmatrix(g,t))
         copy!(S, (I + e^(-β*Δt)*S)*(I + α*A + α^2*A^2) - I)
