f4758d583b823bab0b3a7c9f18b5bb2a46bac2d6
Weijian Zhang <weijian.z@hotmail.com>
2015-06-03 17:01:51 +0100

    rm old egreader

--- src/io.jl
+++ src/io.jl
@@ -1,57 +1,8 @@
-# input data from a text file with each row is of the form 
-# i    j   [w]  timestamp
 
-"""
-Read the contents of the Evolving Graph format file into a 
-EvolvingGraph type object. If info is true, only return information 
-about the number of nodes, the number of edges and the number of timestamps.
-"""
-function egreader(filename, info::Bool = false)
-    egfile = open(filename, "r")
-    # Read the first line
-    firstline = chomp(readline(egfile))
-    tokens = split(firstline)
-    if tokens[1] != "%%EvolvingGraph"
-        throw(ParseError("Not a valid EvolvingGraph header"))
-    end
-    (object, nodetype, timetype) = map(lowercase, tokens[2:4])
-    is_directed = object == "directed" ? true : false
-    
-
-    # skip all comments and empty lines
-    ll = readline(egfile)
-    while length(chomp(ll)) == 0 || (length(ll) > 0 && ll[1] == '%')
-        ll = readline(egfile)
-    end
-
-    dd = map(x -> parse(Int, x), split(ll))
-    if length(dd) < 3
-        throw(ParseError(string("Could not read in EvolvingGraph dimensions from line: ", ll)))
-    end
-    edges = dd[1]
-    nodes = dd[2]
-    times = dd[3]
-
-    if info
-        return (nodes, edges, times)
-    end
-        
-    if nodetype == "integer" && timetype == "integer"
-        ilist = Array(Int, edges)
-        jlist = Array(Int, edges)
-        timestamps = Array(Int, edges)
-        for i in 1:edges
-            entries = split(readline(egfile))
-            ilist[i] = parse(Int, entries[1])
-            jlist[i] = parse(Int, entries[2])
-            timestamps[i] = parse(Int, entries[3])
-        end
-        return EvolvingGraph(is_directed, ilist, jlist, timestamps) 
-    end
-    
-end
-
-function egread(filename, info::Bool =false)
+@doc doc"""
+`egread(filename)` read the contents of an Evolving Graph format file. 
+"""->
+function egread(filename)
     file = open(filename, "r")
     firstline = chomp(readline(file))
     tokens = split(firstline)
