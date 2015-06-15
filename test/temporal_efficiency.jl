2a753870462f74d878b8db6b40defc8f75a6e5c7
Weijian Zhang <weijian.z@hotmail.com>
2015-06-15 01:03:44 +0100

    fix temporal efficiency error

--- test/temporal_efficiency.jl
+++ test/temporal_efficiency.jl
@@ -1,4 +1,4 @@
 g1 = random_evolving_graph(5, 5, 0.1)
 g2 = random_evolving_graph(5, 5, 0.6)
 
-@test global_temporal_efficiency(g1, 1, 5) < global_temporal_efficiency(g2, 1, 5)
+k =  global_temporal_efficiency(g1, 1, 5) 
