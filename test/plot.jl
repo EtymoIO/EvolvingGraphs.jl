08e867eff572843cf87c2f526234ee0b9bdcb091
Weijian Zhang <weijian.z@hotmail.com>
2015-11-14 20:25:04 +0000

    test plotting only for Julia v0.3.x and v0.4.x

--- test/plot.jl
+++ test/plot.jl
@@ -1,8 +1,10 @@
-using Colors
-using Compose
+if VERSION < v"0.5"
+    using Colors
+    using Compose
 
-g = random_evolving_graph(4, 3, 0.5)
-plot(g, 1)
-plot(g, 2)
-save_svg(g, 2, filename="test.svg")
-rm("test.svg")
+    g = random_evolving_graph(4, 3, 0.5)
+    plot(g, 1)
+    plot(g, 2)
+    save_svg(g, 2, filename="test.svg")
+    rm("test.svg")
+end
