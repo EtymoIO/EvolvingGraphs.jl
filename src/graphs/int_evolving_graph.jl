89594e52e1b64bd7bf0eac4bf82357ea41d7c036
Weijian Zhang <weijian.z@hotmail.com>
2016-03-02 01:02:01 -0500

    reimplement IntEvolvingGraph type and related functions

--- src/graphs/int_evolving_graph.jl
+++ src/graphs/int_evolving_graph.jl
@@ -1,32 +1,36 @@
-export int_evolving_graph
-
-typealias IntTuple2 Tuple{Int, Int}
-typealias IntTimeEdge TimeEdge{Int, Int}
+export int_evolving_graph, temporal_nodes
 
 
-type IntEvolvingGraph <: AbstractEvolvingGraph{Int}
+type IntEvolvingGraph <: AbstractEvolvingGraph
     is_directed::Bool
-    nodes::Vector{IntTuple2}
+    nodes::UnitRange{Int}
     timestamps::Vector{Int}
-    nedges::Int
-    # edges: timestamp -> [(source, target, timestamp),...]
-    edges::Dict{Int, Vector{IntTimeEdge}} 
-    # adjlist: (node, timestamp) -> [(node, timestamp),...]
-    adjlist::Dict{IntTuple2, Vector{IntTuple2}}
+    nnodes::Int      # number of nodes
+    nedges::Int      # number of edges
+    forward_adjlist::Vector{Vector{Int}}
+    backward_adjlist::Vector{Vector{Int}}
 end
 
 """
-   int_evolving_graph(;is_directed)
+   int_evolving_graph(nv, nt; is_directed)
 
-Initialize an evolving graph.
+Initialize an evolving graph with `nv` nodes and `nt` timestamps
 """
-int_evolving_graph(;is_directed::Bool = true) = 
-IntEvolvingGraph(is_directed, 
-                 IntTuple2[],
-                 Int[],
-                 0,
-                 Dict{Int, Vector{IntTimeEdge}}(),
-                 Dict{IntTuple2, Vector{IntTuple2}}())
+function int_evolving_graph(nv::Int, nt::Int; is_directed::Bool = true)
+    ts = Array(Int, nv*nt)
+    f_adj = Vector{Int}[]
+    b_adj = Vector{Int}[]
+    for i = 1:nv*nt
+        push!(f_adj, Int[])
+        push!(b_adj, Int[])
+    end
+    for t = 1:nt
+        for v = 1:nv
+            ts[v + nv*(t-1)] = t
+        end
+    end
+    IntEvolvingGraph(is_directed, 1:nv*nt, ts, nv, 0, f_adj, b_adj)
+end
 
 """
     int_evolving_graph(g)
@@ -41,139 +45,79 @@ function int_evolving_graph(g::EvolvingGraph)
     g1
 end
 
-nodes(g::IntEvolvingGraph) = unique(map(x -> x[1], g.nodes))
-num_nodes(g::IntEvolvingGraph) = length(nodes(g))
-
+# all active nodes of g 
+function temporal_nodes(g::IntEvolvingGraph)
+    ns = Array(Tuple{Int, Int}, length(g.nodes))
+    b = g.nnodes
+    for i in g.nodes
+        t = g.timestamps[i]
+        ns[i] = (i - b*(t-1), t)
+    end
+    ns
+end
+nodes(g::IntEvolvingGraph) = 1:g.nnodes
+num_nodes(g::IntEvolvingGraph) = g.nnodes
 num_edges(g::IntEvolvingGraph) = g.nedges
-
-edges(g::IntEvolvingGraph, t::Int) = g.edges[t]
-num_edges(g::IntEvolvingGraph, t::Int) = length(edges(g, t))
+timestamps(g::IntEvolvingGraph) = unique(g.timestamps)
+num_timestamps(g::IntEvolvingGraph) = round(Int, length(g.timestamps)/g.nnodes)
+num_edges(g::IntEvolvingGraph, t::Int) = g.nedges
 
 copy(g::IntEvolvingGraph) = IntEvolvingGraph(is_directed(g), 
                                              deepcopy(g.nodes), 
                                              deepcopy(g.timestamps), 
-                                             deepcopy(g.nedges), 
-                                             deepcopy(g.edges), 
-                                             deepcopy(g.adjlist))
+                                             g.nnodes,
+                                             g.nedges, 
+                                             deepcopy(g.forward_adjlist))
 
 
-function edges(g::IntEvolvingGraph)
-    elist = IntTimeEdge[]
-    for t in timestamps(g)
-        append!(elist, edges(g, t))
+
+function forward_neighbors(g::IntEvolvingGraph, v::Int, t::Int)
+    ns = g.nnodes
+    n = v + ns*(t-1)
+    nn = Tuple{Int, Int}[]
+    for node in g.forward_adjlist[n]
+        t = g.timestamps[node]
+        v1 = node - ns*(t-1)
+        push!(nn, (v1, t))
     end
-    elist
+    nn
 end
+forward_neighbors(g::IntEvolvingGraph, v::Tuple) = forward_neighbors(g, v[1], v[2])
 
 
-
-function out_neighbors(g::IntEvolvingGraph, v::Tuple)
-    try 
-        return g.adjlist[v]
-    catch KeyError
-        return collect(zip([], []))
+function add_edge!(g::IntEvolvingGraph, v1::Int, v2::Int, t::Int)
+    ns = g.nnodes
+    n1 = v1 + ns*(t-1)
+    n2 = v2 + ns*(t-1)
+    if n2 in g.forward_adjlist[n1]
+        error("$(n2) is already in the forward neighbors.")
+    else
+        push!(g.forward_adjlist[n1], n2)
     end
-end
-out_neighbors(g::IntEvolvingGraph, v, t) = out_neighbors(g, (int(v), int(t)))
-
-has_node(g::IntEvolvingGraph, v::IntTuple2) = (v in g.nodes)
-
-timestamps(g::IntEvolvingGraph) = g.timestamps
-
-function add_node!(g::IntEvolvingGraph, v::IntTuple2)
-    if !(v in g.nodes)
-        push!(g.nodes, v)
-        if !(v[2] in keys(g.edges))
-             g.edges[v[2]] = IntTimeEdge[]
-        end
-
-        g.adjlist[v] = IntTuple2[]
-   
-        if !(v[2] in g.timestamps)
-            push!(g.timestamps, v[2])
-            sort!(g.timestamps)
-        end
+    if n1 in g.backward_adjlist[n2]
+        error("$(n1) is already in the backward neighbors")
+    else
+        push!(g.backward_adjlist[n2], n1)
     end
-    v
-end
-
-function add_edge!(g::IntEvolvingGraph, v1::IntTuple2, v2::IntTuple2)
-    v1[2] == v2[2] || error("Input nodes $(v1[1]) and $(v2[1]) must at the same timestamps")
-    v1 in g.nodes || add_node!(g, v1)
-    v2 in g.nodes || add_node!(g, v2)
-
-    index1 = findin((map(x -> x[1], g.nodes)), v1[1])
-    for node in g.nodes[index1]
-        if node < v1
-            v1 in g.adjlist[node] || push!(g.adjlist[node], v1)
+    g.nedges += 1
+    for (v, n) in ((v1, n1), (v2, n2))
+        for i in 1:t-1
+            len = length(g.forward_adjlist[v + ns*(i-1)]) + 
+            length(g.backward_adjlist[v + ns*(i-1)])       
+            if len > 0
+                n in g.forward_adjlist[v + ns*(i-1)] || push!(g.forward_adjlist[v + ns*(i-1)], n) 
+                (v + ns*(i-1)) in g.backward_adjlist[n] || push!(g.backward_adjlist[n], v + ns*(i-1))
+            end
         end
-    end
-    index2 = findin((map(x -> x[1], g.nodes)), v2[1])
-    for node in g.nodes[index2]
-        if node < v2
-            v2 in g.adjlist[node] || push!(g.adjlist[node], v2)
-        end
-    end
-
-    if !(v2 in g.adjlist[v1]) 
-        push!(g.adjlist[v1], v2)
-        push!(g.edges[v1[2]], TimeEdge(v1[1], v2[1], v1[2]))
-        if !(g.is_directed)
-            push!(g.adjlist[v2], v1)
-            push!(g.edges[v1[2]], TimeEdge(v2[1], v1[1], v1[2]))
-        end
-        g.nedges += 1
+        #for i in t+1:num_timestamps(g)
+         #   println("node: $(v + ns*(i-1))")
+          #  len = length(g.forward_adjlist[v + ns*(i-1)]) + 
+          #  length(g.backward_adjlist[v + ns*(i-1)])
+          #  if len > 0
+           #     push!(g.forward_adjlist[n], v+ ns*(i-1))
+           #     push!(g.backward_adjlist[v + ns*(i-1)], n)
+           # end
+       # end
     end
     g
-end
-
-add_edge!(g::IntEvolvingGraph, v1::Int, v2::Int, t::Int) = add_edge!(g, (v1, t), (v2, t))
-add_edge!(g::IntEvolvingGraph, te::TimeEdge{Int, Int}) = add_edge!(g, source(te), target(te), timestamp(te))
-
-
-function undirected!(g::IntEvolvingGraph)
-    for t in timestamps(g)
-        for e in edges(g, t)
-            add_edge!(g, target(e), source(e), timestamp(e))
-        end
-    end
-    g.is_directed = false
-    g
-end
-            
-function slice(g::IntEvolvingGraph, t1::Int, t2::Int)
-    ts = timestamps(g)
-    st = findfirst(ts, t1) 
-    ed = findfirst(ts, t2)
-    g1 = evolving_graph()
-    for t in ts[st:ed]
-        for te in edges(g, t)
-            add_edge!(g1, te)
-        end
-    end
-    g1
-end
-
-function spmatrix(g::IntEvolvingGraph, t::Int)
-    es = edges(g, t)
-    n = length(es)
-    dim = maximum(nodes(g))
-    is = Array(Int, n)
-    js = Array(Int, n)
-    vs = ones(Bool, n)
-    for (i,e) in enumerate(es)
-        is[i] = source(e)
-        js[i] = target(e)
-    end
-    return sparse(is, js, vs, dim, dim)
-end
-
-function matrix(g::IntEvolvingGraph, t::Int)
-    es = edges(g, t)
-    dim = maximum(nodes(g))
-    A = zeros(Bool, dim, dim)
-    for e in es
-        A[source(e), target(e)] = true
-    end
-    A
 end
