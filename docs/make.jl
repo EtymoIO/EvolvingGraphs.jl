1fccffddb2389f577f90553be5ed48e377caeb15
Weijian Zhang <weijian@etymo.io>
2018-01-18 23:45:15 +0000

    add deploydocs julia version = 0.6

--- docs/make.jl
+++ docs/make.jl
@@ -4,11 +4,12 @@ makedocs(
     format=:html,
     sitename="EvolvingGraphs.jl",
     modules=[EvolvingGraphs, EvolvingGraphs.Centrality],
-    pages=["base.md", "graph_types.md", "centrality.md", "examples.md", "read_write.md"]
+    pages=["index.md", "base.md", "graph_types.md", "centrality.md", "examples.md", "read_write.md"]
 )
 
 deploydocs(
     branch = "gh-pages",
     latest = "master",
+    julia = "0.6",
     repo = "EtymoIO/EvolvingGraphs.jl"
 )
