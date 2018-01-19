e3926545593be3aba05198a196d4d0d6a3d031e8
autodocs <autodocs>
2018-01-19 00:39:26 +0000

    build based on 0aa772e

---
+++ latest/search_index.js
@@ -0,0 +1,91 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#EvolvingGraphs-Documentation-1",
+    "page": "Home",
+    "title": "EvolvingGraphs Documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#-1",
+    "page": "Home",
+    "title": "",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#Index-1",
+    "page": "Home",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "base.html#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "graph_types.html#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "centrality.html#",
+    "page": "Centrality Algorithms",
+    "title": "Centrality Algorithms",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "centrality.html#EvolvingGraphs.Centrality.katz",
+    "page": "Centrality Algorithms",
+    "title": "EvolvingGraphs.Centrality.katz",
+    "category": "Function",
+    "text": "katz(g [, α = 0.3, sorted = true])\n\nComputes the broadcast centrality vector of an evolving graph g.\n\nInput:\n\n `g`: an evolving graph\n `α`: (= 0.3 default) a scalar the controls the influence of long walks.\n\n\n\nkatz(g, α, β [, mode = :broadcast])\n\nComputes the Katz centrality of an evolving graph g.\n\nInput:\n\n `g`: an evolving graph\n `α`: a scalar that controls the influence of long walks\n `β`: a scalar that controls the influence of walks happened long time ago.\n      `mode`: `mode = :broadcast` (default) generates the broadcast centrality\n      vector; `mode = :receive` generates the receving centrality vector;\n      `mode = :matrix` generates the communicability matrix.\n\n\n\n"
+},
+
+{
+    "location": "centrality.html#Centrality-Algorithms-1",
+    "page": "Centrality Algorithms",
+    "title": "Centrality Algorithms",
+    "category": "section",
+    "text": "katz"
+},
+
+{
+    "location": "examples.html#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "read_write.html#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": ""
+},
+
+]}
