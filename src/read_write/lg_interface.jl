07b1bbf7b095ce0dbc44a5ad6dc683bb6e0868c4
mbesancon <mathieu.besancon@gmail.com>
2018-04-18 15:38:35 -0400

    passing add_edge

--- src/read_write/lg_interface.jl
+++ src/read_write/lg_interface.jl
@@ -1,36 +1,36 @@
-LightGraphs.nv(g::AbstractGraph) = num_nodes(g)
-LightGraphs.ne(g::AbstractGraph) = num_edges(g)
-LightGraphs.is_directed(g::AbstractGraph) = is_directed(g)
+LightGraphs.nv(g::AbstractGraph{V,E}) where {V,E} = num_nodes(g)
+LightGraphs.ne(g::AbstractGraph{V,E}) where {V,E} = num_edges(g)
+LightGraphs.is_directed(g::AbstractGraph{V,E}) where {V,E} = is_directed(g)
 
-function LightGraphs.outneighbors(g::AbstractStaticGraph{V}, v::V) where V
+function LightGraphs.outneighbors(g::AbstractStaticGraph{V,E}, v::V) where {V,E}
     edges = out_edges(g, v)
     return Node{V}[target(e) for e in edges]
 end
 
-function LightGraphs.inneighbors(g::AbstractStaticGraph{V}, v::V) where V
+function LightGraphs.inneighbors(g::AbstractStaticGraph{V,E}, v::V) where {V,E}
     edges = in_edges(g, v)
     return Node{V}[source(e) for e in edges]
 end
 
-LightGraphs.src(e::AbstractEdge{V}) where V = source(e) 
-LightGraphs.dst(e::AbstractEdge{V}) where V = target(e) 
+LightGraphs.src(e::AbstractEdge{V}) where V = source(e)
+LightGraphs.dst(e::AbstractEdge{V}) where V = target(e)
 
-LightGraphs.has_edge(g::AbstractGraph{V}, e::AbstractEdge{V}) where V = has_edge(g,e)
-LightGraphs.has_vertex(g::AbstractGraph{V}, n::AbstractNode{V}) where V = has_node(g,n)
+LightGraphs.has_edge(g::AbstractGraph{V,E}, e::AbstractEdge{V}) where {V,E} = has_edge(g,e)
+LightGraphs.has_vertex(g::AbstractGraph{V,E}, n::AbstractNode{V}) where {V,E} = has_node(g,n)
 
-function Base.reverse(e::AbstractEdge{V}) where V = edge_reverse(e)
+Base.reverse(e::AbstractEdge{V}) where V = edge_reverse(e)
 
-LightGraphs.edges(g::AbstractGraph{V}) where V = edges(g)
-LightGraphs.edgetype(::AbstractGraph{V}) where V = AbstractEdge{V}
+LightGraphs.edges(g::AbstractGraph{V,E}) where {V,E} = edges(g)
+LightGraphs.edgetype(::AbstractGraph{V,E}) where {V,E} = AbstractEdge{V}
 
-LightGraphs.vertices(g::AbstractGraph{V}) where V = nodes(g)
+LightGraphs.vertices(g::AbstractGraph{V,E}) where {V,E} = nodes(g)
 
-LightGraphs.add_edge!(g::AbstractStaticGraph{V}, e::AbstractEdge{V}) where V = add_edge!(g, source(e), dst(e))
-LightGraphs.add_edge!(g::AbstractEvolvingGraph{V}, e::TimeEdge{V,T}) where {V, T} = add_edge!(g, source(e), dst(e), e.timestamp)
-LightGraphs.add_edge!(g::AbstractEvolvingGraph{V}, e::TimeWeightedEdge{V,T,W}) where {V,T,W} = 
+LightGraphs.add_edge!(g::AbstractStaticGraph{V}, e::AbstractEdge{ET}) where {V,ET} = add_edge!(g, LightGraphs.src(e), LightGraphs.dst(e))
+LightGraphs.add_edge!(g::AbstractEvolvingGraph{V,E,T}, e::TimeEdge{ET}) where {V, E, T, ET} = add_edge!(g, LightGraphs.src(e), LightGraphs.dst(e), e.timestamp)
+LightGraphs.add_edge!(g::AbstractEvolvingGraph{V,E,T}, e::WeightedTimeEdge{EV,ET,EW}) where {V,E,T,EV,ET,EW} = 
     add_edge!(g, source(v1), target(v2), e.timestamp, weight = e.weight)
 
-function LightGraphs.add_vertex!(g::AbstractStaticGraph{V}) where V
+function LightGraphs.add_vertex!(g::AbstractStaticGraph{V,E}) where {V,E}
     nnodes = num_nodes(g)
     add_node!(g,nnodes+1)
 end
--- test/lg_interface.jl
+++ test/lg_interface.jl
@@ -8,5 +8,6 @@ add_edge!(g, "a", "b")
 
 
 @test LightGraphs.nv(g) == 2
+@test LightGraphs.ne(g) == 1
+LightGraphs.add_edge!(g, Edge("b", "a"))
 @test LightGraphs.ne(g) == 2
-
